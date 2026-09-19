e379153fac6c0b45225173daa0578c5a76f6c8b9
bhavanaparamesh <pullalarevubhavana@gmail.com>
2026-09-19 10:14:16 +0530

    Update Jenkins installation script for repository URLs

--- jenkins.sh
+++ jenkins.sh
@@ -2,14 +2,14 @@
 yum install git  -y
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
 
 #STEP-3: DOWNLOAD JAVA11 AND JENKINS
 yum install java-21-amazon-corretto -y
 yum install jenkins -y
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
-systemctl status jenkins.service
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
